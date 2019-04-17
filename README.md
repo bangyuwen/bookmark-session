9c735b2873338b213e26ce181690501f7353b44c
BangYuWen <bangyuwen@gmail.com>
2019-04-17 09:18:41 +0800

    trying different api

---
+++ README.md
@@ -0,0 +1,11 @@
+# bookmark-session
+
+## problem we are facing
+
+1. Moving tabs between full screen windows
+2. Can't get session record when switching computer
+
+## function we need
+
+1. auto align by url
+2. auto align by title
--- src/main.js
+++ src/main.js
@@ -11,16 +11,19 @@ const toTitle = (node) => {
 
 chrome.bookmarks.getTree(
   (bookmarkTreeNodes) => {
-    
+
     console.log(bookmarkTreeNodes);
     const titleBookmarks = toTitle(bookmarkTreeNodes[0]);
     console.log(titleBookmarks);
-    
+
     const text = JSON.stringify(titleBookmarks, null, 4);
     display(text);
   },
 );
 
+chrome.system.memory.getInfo((info) => {
+  console.log(info);
+});
 // chrome.bookmarks.create({
 //   title: 'test',
 //   url: 'chrome://bookmarks/?id=2',
@@ -31,3 +34,8 @@ chrome.bookmarks.getTree(
 // chrome.bookmarks.get('2', (bookmark) => {
 //   console.log(bookmark);
 // });
+
+chrome.tabs.query({}, (tabs) => {
+  console.log(tabs);
+  
+});
